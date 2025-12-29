7a2851ffe351aa515f289a8bec4f22f5dbf214d1
JohnnyMatheus <alunotioficial@gmail.com>
2025-12-29 02:38:08 -0300

    Update funcao.js

--- funções/funcao.js
+++ funções/funcao.js
@@ -6,19 +6,29 @@ console.log(areaQuadrado(2,2));
 
 console.log('///////////////////////////////////////////');
 //PARAMETROS E ARGUMENTOS
-
+console.log('CALUCULAR IMC')
 function imc(peso,altura){
     var imc = peso /altura **2;
-   if(imc <= 19.0){
-    return(`Abaixo do Peso: ${imc}`)
-   }else if (imc >=19.1 && imc<=25.8){
-    return(`Peso normal: ${imc}`)
-   }else if(){
-    
-   }
-   return imc;
+      if(imc <= 19.0){
+         return(`Abaixo do Peso: ${imc}`)
+      }else if (imc >=19.1 && imc<=25.8){
+         return(`Peso normal: ${imc}`)
+      }else if(imc >=25.9 && imc<=27.3){
+         return(`Pouco Acima do Peso: ${imc}`)
+      }else if(imc >=24.4 && imc<=32.3){
+         return(`Acima do Peso ${imc}`)
+      }else{
+         return(`Obesidade ${imc.toFixed(2)}`)
+      }
+  
 }
 
-console.log(imc(80, 1.85));
+console.log(imc(130, 1.85));
 
+console.log('///////////////////////////////////////////');
+console.log('COR FAVORITA')
+
+function corFavorita(){
+   if
+}
 
